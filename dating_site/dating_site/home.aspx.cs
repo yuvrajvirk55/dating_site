c1b1c8d7b85f356b887a65920def32e4d52281cf
Yuvraj Singh <uvnemk@gmail.com>
2018-05-31 15:38:50 -0700

    working on db

--- dating_site/dating_site/home.aspx.cs
+++ dating_site/dating_site/home.aspx.cs
@@ -6,6 +6,7 @@ using System.Web.UI;
 using System.Web.UI.WebControls;
 using System.Data.SqlClient;
 using System.Configuration;
+using System.Data;
 
 namespace dating_site
 {
@@ -18,36 +19,13 @@ namespace dating_site
 
         protected void ImageButton2_Click(object sender, ImageClickEventArgs e)
         {
-          
-           SqlConnection conn = new SqlConnection(ConfigurationManager.ConnectionStrings["userdataConnectionString"].ConnectionString);
-            conn.Open();
-            string checkemail= "select count(*) from usertable where email='"+ textbox_email.Text+ "'";
-            SqlCommand com = new SqlCommand(checkemail, conn);
 
-            if (checkemail.ToString() != textbox_email.ToString())
-                {
-                 Response.Write("user doesnot exist");
-               
-            }
+               SqlConnection conn = new SqlConnection(ConfigurationManager.ConnectionStrings["userdataConnectionString"].ConnectionString);
+               conn.Open();
 
-            else
+               string checkemail= "select email from usertable where email= "+ textbox_email.Text;
 
-            {
-                string  checkpassword = "select password from usertable where email='" + checkemail + "'";
-                SqlCommand com1 = new SqlCommand(checkpassword, conn);
-                if (checkpassword.Equals(textbox_password))
-                {
-                    Response.Write("login successful");
-                  
-                }
-                else
-                {
-                    Response.Write("incorrect password");
-                   
-                }
-
-            }
-            conn.Close();
+     
         }
 
         protected void textbox_email_TextChanged(object sender, EventArgs e)
