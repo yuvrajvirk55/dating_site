846eb4b064097db7e6950fd537627f114310465b
Yuvraj Singh <uvnemk@gmail.com>
2018-06-03 14:16:43 -0700

    enabled database

--- dating_site/dating_site/home.aspx.cs
+++ dating_site/dating_site/home.aspx.cs
@@ -24,21 +24,45 @@ namespace dating_site
             int index = filtered_email.IndexOf('@');
             filtered_email = filtered_email.Substring(0, index);
 
+            string checkemail = "select email from usertable WHERE email LIKE '%" + filtered_email + "%' ";
+            string checkpassword = "select password from usertable WHERE password = '" + textbox_password.Text + "' ";
 
             SqlConnection connection = new SqlConnection(ConfigurationManager.ConnectionStrings["userdataConnectionString"].ConnectionString);
 
-            string checkemail= "select email from usertable WHERE email LIKE %uvnemk% ";
+            SqlCommand command_email = new SqlCommand(checkemail, connection);
 
-            SqlCommand command = new SqlCommand(checkemail, connection);
+            SqlCommand command_password = new SqlCommand(checkpassword, connection);
             connection.Open();
 
-            string tDate = (string)command.ExecuteScalar();
+            string db_email = (string)command_email.ExecuteScalar();
+            string db_password = (string)command_password.ExecuteScalar();
 
-            Response.Write(tDate);
+            Response.Write(db_email+"    "+ db_password+"    "+ textbox_email.Text+"   "+textbox_password.Text);
+
+           
+
+            /*  if (db_email.Equals(textbox_email.Text))
+              {
+                  Response.Write("User exists");
+              }
+
+              else if (!db_email.Equals(textbox_email.Text.ToString()))
+              {
+                  Response.Write("User does not exists");
+              }
+
+              else if (db_password != textbox_password.Text.ToString())
+              {
+                  Response.Write("Invalid password");
+              }
+
+              else
+              {
+                  Response.Write("successful");
+              }
+              */
             connection.Close();
-
-
-
+            
         }
 
         protected void textbox_email_TextChanged(object sender, EventArgs e)
