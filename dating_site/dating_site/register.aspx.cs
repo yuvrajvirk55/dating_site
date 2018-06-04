3c9176297c0c8753fc96679b7359c7abc231998d
Yuvraj Singh <uvnemk@gmail.com>
2018-06-04 06:53:04 -0700

    working on backend

--- dating_site/dating_site/register.aspx.cs
+++ dating_site/dating_site/register.aspx.cs
@@ -18,12 +18,12 @@ namespace dating_site
 
         protected void register_register_Click(object sender, EventArgs e)
         {
-            string[] lines = { register_fname.Text+"     " + register_lname.Text+"   "+ register_email.Text+"    "+ register_password.Text+"    "+register_dob.Text+"    "+register_gender.Text };
+            string[] lines = { register_fname.Text + "     " + register_lname.Text + "   " + register_email.Text + "    " + register_password.Text + "    " + register_dob.Text + "    " + register_gender.Text };
             System.Data.SqlClient.SqlConnection sqlConnection1 = new System.Data.SqlClient.SqlConnection(ConfigurationManager.ConnectionStrings["userdataConnectionString"].ConnectionString);
 
             System.Data.SqlClient.SqlCommand cmd = new System.Data.SqlClient.SqlCommand();
             cmd.CommandType = System.Data.CommandType.Text;
-            cmd.CommandText = "INSERT usertable (email,password,first_name,last_name,dob,sex) VALUES ('new@gmail.com','asdf','honey','singh','10-10-2018','male')";
+            cmd.CommandText = "INSERT usertable (email,password,first_name,last_name,dob,sex) VALUES ('" + register_email.Text + "','" + register_password.Text + "','" + register_fname.Text + "','" + register_lname.Text + "','" + register_dob.Text + "','" + register_gender.Text +"')";
             cmd.Connection = sqlConnection1;
 
             sqlConnection1.Open();
