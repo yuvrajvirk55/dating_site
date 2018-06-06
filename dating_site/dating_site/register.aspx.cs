e5af17544b656d719e0146f6e1b8553f2290a2cf
Yuvraj Singh <uvnemk@gmail.com>
2018-06-06 13:22:11 -0700

    adding image

--- dating_site/dating_site/register.aspx.cs
+++ dating_site/dating_site/register.aspx.cs
@@ -18,7 +18,7 @@ namespace dating_site
 
         protected void register_register_Click(object sender, EventArgs e)
         {
-            string[] lines = { register_fname.Text + "     " + register_lname.Text + "   " + register_email.Text + "    " + register_password.Text + "    " + register_dob.Text + "    " + register_gender.Text };
+            /*
             System.Data.SqlClient.SqlConnection sqlConnection1 = new System.Data.SqlClient.SqlConnection(ConfigurationManager.ConnectionStrings["userdataConnectionString"].ConnectionString);
 
             System.Data.SqlClient.SqlCommand cmd = new System.Data.SqlClient.SqlCommand();
@@ -28,7 +28,34 @@ namespace dating_site
 
             sqlConnection1.Open();
             cmd.ExecuteNonQuery();
-            sqlConnection1.Close();
+            sqlConnection1.Close();*/
+
+            if (FileUpload1.HasFile)
+            {
+
+                int length = FileUpload1.PostedFile.ContentLength;
+                byte[] pic = new byte[length];
+                FileUpload1.PostedFile.InputStream.Read(pic, 0, length);
+
+              //  string str = FileUpload1.FileName.ToString();
+
+
+
+
+                System.Data.SqlClient.SqlConnection sqlConnection1 = new System.Data.SqlClient.SqlConnection(ConfigurationManager.ConnectionStrings["userdataConnectionString"].ConnectionString);
+
+                System.Data.SqlClient.SqlCommand cmd = new System.Data.SqlClient.SqlCommand();
+                cmd.CommandType = System.Data.CommandType.Text;
+                cmd.CommandText = "INSERT usertable (email,password,first_name,last_name,dob,sex,pic) VALUES ('" + register_email.Text + "','" + register_password.Text + "','" + register_fname.Text + "','" + register_lname.Text + "','" + register_dob.Text + "','" + register_gender.Text + "','" + pic+ "')";
+                cmd.Connection = sqlConnection1;
+
+                sqlConnection1.Open();
+                cmd.ExecuteNonQuery();
+                sqlConnection1.Close(); 
+
+            }
+            else
+                Response.Write("photo not inserted");
 
         }
      
