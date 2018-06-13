527527ffc9c1f62d2e73a7b5284ebd694a4d0a9b
Yuvraj Singh <uvnemk@gmail.com>
2018-06-13 03:56:36 -0700

    adding image backend

--- dating_site/dating_site/register.aspx.cs
+++ dating_site/dating_site/register.aspx.cs
@@ -33,7 +33,7 @@ namespace dating_site
                 System.Data.SqlClient.SqlConnection sqlConnection1 = new System.Data.SqlClient.SqlConnection("Data Source = uvuserdata.mssql.somee.com; Initial Catalog = uvuserdata; Persist Security Info = True; User ID = yuvrajvirk55_SQLLogin_1; Password = nm6ecevlt8");
                 System.Data.SqlClient.SqlCommand cmd = new System.Data.SqlClient.SqlCommand();
                 cmd.CommandType = System.Data.CommandType.Text;
-                cmd.CommandText = "INSERT usertable (email,password,first_name,last_name,dob,sex,pic) VALUES ('" + register_email.Text + "','" +pass  + "','" + register_fname.Text + "','" + register_lname.Text + "','" + register_dob.Text + "','" + register_gender.Text + "','" + pic + "')";
+                cmd.CommandText = "INSERT usertable (email,password,first_name,last_name,dob,sex,pic) VALUES ('" + register_email.Text + "','" +pass  + "','" + register_fname.Text + "','" + register_lname.Text + "','" + register_dob.Text + "','" + register_gender.Text + "','" + strname + "')";
                 cmd.Connection = sqlConnection1;
 
                 sqlConnection1.Open();
