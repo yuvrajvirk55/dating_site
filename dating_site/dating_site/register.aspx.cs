c138f8455b39fcc0371b9353d5f661702c2ef9a4
Yuvraj Singh <uvnemk@gmail.com>
2018-06-13 16:24:24 -0700

    image uploaded

--- dating_site/dating_site/register.aspx.cs
+++ dating_site/dating_site/register.aspx.cs
@@ -19,9 +19,23 @@ namespace dating_site
 
         protected void register_register_Click(object sender, EventArgs e)
         {
-             string strname = FileUpload1.FileName.ToString();
-          
-             FileUpload1.PostedFile.SaveAs(Server.MapPath("~/upload/") + register_fname.Text+"_"+ register_lname.Text +"-"+strname);
+            if (!FileUpload1.HasFile)
+            {
+                Page.ClientScript.RegisterClientScriptBlock(typeof(Page), "Alert", "alert('Upload image')", true);
+            }
+
+            else
+            {
+                string filtered_email = register_email.Text.ToString();
+                int index = filtered_email.IndexOf('@');
+                filtered_email = filtered_email.Substring(0, index);
+
+
+                string strname = FileUpload1.FileName.ToString();
+
+                FileUpload1.PostedFile.SaveAs(Server.MapPath("~/upload/") + filtered_email + "-" + strname);
+
+
 
                 //encrpting pass
 
@@ -30,18 +44,19 @@ namespace dating_site
                 System.Data.SqlClient.SqlConnection sqlConnection1 = new System.Data.SqlClient.SqlConnection("Data Source = uvuserdata.mssql.somee.com; Initial Catalog = uvuserdata; Persist Security Info = True; User ID = yuvrajvirk55_SQLLogin_1; Password = nm6ecevlt8");
                 System.Data.SqlClient.SqlCommand cmd = new System.Data.SqlClient.SqlCommand();
                 cmd.CommandType = System.Data.CommandType.Text;
-                cmd.CommandText = "INSERT usertable (email,password,first_name,last_name,dob,sex,pic) VALUES ('" + register_email.Text + "','" +pass  + "','" + register_fname.Text + "','" + register_lname.Text + "','" + register_dob.Text + "','" + register_gender.Text + "','" + strname + "')";
+                cmd.CommandText = "INSERT usertable (email,password,first_name,last_name,dob,sex,pic) VALUES ('" + register_email.Text + "','" + pass + "','" + register_fname.Text + "','" + register_lname.Text + "','" + register_dob.Text + "','" + register_gender.Text + "','" + strname + "')";
                 cmd.Connection = sqlConnection1;
 
                 sqlConnection1.Open();
                 cmd.ExecuteNonQuery();
                 sqlConnection1.Close();
 
-            Session["email"] = register_email.Text;
-            Session["fname"] = register_fname.Text;
-            Session["lname"] = register_lname.Text;
-            Session["img"] = strname;
-            Response.Redirect("form.aspx");
+                Session["email"] = register_email.Text;
+                Session["fname"] = register_fname.Text;
+                Session["lname"] = register_lname.Text;
+                Session["img"] = strname;
+                Response.Redirect("form.aspx");
+            }
             
         }
 
