604f9cf3828a9d8be3b5b3f43f867503fc68ce16
Yuvraj Singh <uvnemk@gmail.com>
2018-06-20 04:53:48 -0700

    adding radio table

--- dating_site/dating_site/WebForm1.aspx
+++ dating_site/dating_site/WebForm1.aspx
@@ -10,7 +10,22 @@
     <form id="form1" runat="server">
     <div>
     
+        <asp:RadioButtonList ID="RadioButtonList1" runat="server">
+            <asp:ListItem>usertable</asp:ListItem>
+            <asp:ListItem>userinterest</asp:ListItem>
+        </asp:RadioButtonList>
+    
     </div>
+        <asp:GridView ID="GridView1" runat="server" style="position: relative; top: 0px; left: 0px; height: 209px; width: 352px">
+        </asp:GridView>
+        <p>
+            <asp:TextBox ID="TextBox1" runat="server" Height="29px" Width="167px"></asp:TextBox>
+&nbsp; where
+            <asp:TextBox ID="TextBox2" runat="server" Height="39px" Width="215px"></asp:TextBox>
+        </p>
+        <p>
+            <asp:Button ID="Button1" runat="server" Height="50px" OnClick="Button1_Click" style="position: relative" Text="Button" Width="141px" />
+        </p>
     </form>
 </body>
 </html>
--- dating_site/dating_site/WebForm1.aspx.cs
+++ dating_site/dating_site/WebForm1.aspx.cs
@@ -4,6 +4,8 @@ using System.Linq;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
+using System.Data;
+using System.Data.SqlClient;
 
 namespace dating_site
 {
@@ -11,6 +13,32 @@ namespace dating_site
     {
         protected void Page_Load(object sender, EventArgs e)
         {
+
+        }
+
+        protected void Button1_Click(object sender, EventArgs e)
+        {
+            SqlConnection con = new SqlConnection("Data Source = uvuserdata.mssql.somee.com; Initial Catalog = uvuserdata; Persist Security Info = True; User ID = yuvrajvirk55_SQLLogin_1; Password = nm6ecevlt8");
+            con.Open();
+
+            SqlCommand cmd;
+
+            if (RadioButtonList1.Text == "usertable")
+            {
+                 cmd = new SqlCommand("select " + TextBox1.Text.ToString() + " from usertable", con);
+            }
+            else
+            {
+                 //cmd = new SqlCommand("select " + TextBox1.Text.ToString() + " from usertable", con);
+            }
+
+            SqlDataAdapter da = new SqlDataAdapter(cmd);
+            DataSet ds = new DataSet();
+            da.Fill(ds);
+            cmd.ExecuteNonQuery();
+            GridView1.DataSource = ds;
+            GridView1.DataBind();
+
 
         }
     }
