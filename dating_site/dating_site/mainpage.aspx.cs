fa7dc2f6c056365bc0cc597c55aad3798ea3a9be
Yuvraj Singh <uvnemk@gmail.com>
2018-06-23 20:46:53 -0700

    added table

--- dating_site/dating_site/mainpage.aspx.cs
+++ dating_site/dating_site/mainpage.aspx.cs
@@ -25,7 +25,7 @@ namespace dating_site
 
             SqlCommand cmd;
 
-            cmd = new SqlCommand("SELECT first_name + ' ' + last_name AS name, int_sex as gender,in_hobbies as hobbies,pic FROM usertable INNER JOIN userinterest ON usertable.email=userinterest.email where first_name + ' ' + last_name LIKE '%admin%'", con);
+            cmd = new SqlCommand("SELECT first_name + ' ' + last_name AS name, int_sex as gender,in_hobbies as hobbies,pic FROM usertable INNER JOIN userinterest ON usertable.email=userinterest.email where first_name + ' ' + last_name LIKE '" + searchbar_text.Text.ToString() + "%'", con);
             SqlDataAdapter da = new SqlDataAdapter(cmd);
             DataSet ds = new DataSet();
             try
