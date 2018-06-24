91418d9588297be8717ff61c94552a285152f768
Yuvraj Singh <uvnemk@gmail.com>
2018-06-24 04:12:31 -0700

    adding code to check

--- dating_site/dating_site/WebForm1.aspx.designer.cs
+++ dating_site/dating_site/WebForm1.aspx.designer.cs
@@ -13,15 +13,6 @@ namespace dating_site {
     public partial class WebForm1 {
         
         /// <summary>
-        /// form1 control.
-        /// </summary>
-        /// <remarks>
-        /// Auto-generated field.
-        /// To modify move field declaration from designer file to code-behind file.
-        /// </remarks>
-        protected global::System.Web.UI.HtmlControls.HtmlForm form1;
-        
-        /// <summary>
         /// GridView1 control.
         /// </summary>
         /// <remarks>
@@ -40,12 +31,12 @@ namespace dating_site {
         protected global::System.Web.UI.WebControls.Button Button1;
         
         /// <summary>
-        /// Label1 control.
+        /// lblResult control.
         /// </summary>
         /// <remarks>
         /// Auto-generated field.
         /// To modify move field declaration from designer file to code-behind file.
         /// </remarks>
-        protected global::System.Web.UI.WebControls.Label Label1;
+        protected global::System.Web.UI.WebControls.Label lblResult;
     }
 }
