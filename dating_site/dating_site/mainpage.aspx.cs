c9747285fe21a810d4956a310ba936f4a5ff7fba
Yuvraj Singh <uvnemk@gmail.com>
2018-06-29 06:24:30 -0700

    editing page

--- dating_site/dating_site/mainpage.aspx.cs
+++ dating_site/dating_site/mainpage.aspx.cs
@@ -57,7 +57,7 @@ namespace dating_site
 
             SqlCommand cmd;
 
-            cmd = new SqlCommand("SELECT userinterest.email as email, first_name + ' ' + last_name AS name, int_sex as gender,in_hobbies as hobbies,pic FROM usertable INNER JOIN userinterest ON usertable.email=userinterest.email'", con);
+            cmd = new SqlCommand("SELECT userinterest.email as email, first_name + ' ' + last_name AS name, int_sex as gender,in_hobbies as hobbies,pic FROM usertable INNER JOIN userinterest ON usertable.email=userinterest.email", con);
             SqlDataAdapter da = new SqlDataAdapter(cmd);
             DataSet ds = new DataSet();
             try
