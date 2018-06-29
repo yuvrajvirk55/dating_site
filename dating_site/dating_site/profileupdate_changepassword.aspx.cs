13caecf2d1a32f0c43948f848f856d28f277062b
Yuvraj Singh <uvnemk@gmail.com>
2018-06-29 05:50:21 -0700

    editting page

--- dating_site/dating_site/profileupdate_changepassword.aspx.cs
+++ dating_site/dating_site/profileupdate_changepassword.aspx.cs
@@ -23,9 +23,24 @@ namespace dating_site
                 int index = filtered_email.IndexOf('@');
                 filtered_email = filtered_email.Substring(0, index);
 
+                //encrpting pass
+                string pass = Encrypt.EncryptString(register_password.Text, register_password.Text);
+
+
+                System.Data.SqlClient.SqlConnection sqlConnection1 = new System.Data.SqlClient.SqlConnection("Data Source = uvuserdata.mssql.somee.com; Initial Catalog = uvuserdata; Persist Security Info = True; User ID = yuvrajvirk55_SQLLogin_1; Password = nm6ecevlt8");
+                System.Data.SqlClient.SqlCommand cmd = new System.Data.SqlClient.SqlCommand();
+
+                cmd.CommandType = System.Data.CommandType.Text;
+                cmd.CommandText = "UPDATE usertable SET password = '" + pass + "') where email LIKE '" + filtered_email + "%'";
+                cmd.Connection = sqlConnection1;
+
+                sqlConnection1.Open();
+                cmd.ExecuteNonQuery();
+
+                sqlConnection1.Close();
+
+
             }
-
-
 
             catch
             {
