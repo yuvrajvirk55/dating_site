fbe899fb2826c5a7550d1c5f16d7c3e74060e41b
Yuvraj Singh <uvnemk@gmail.com>
2018-06-29 05:56:20 -0700

    edititng page

--- dating_site/dating_site/profileupdate_interests.aspx.cs
+++ dating_site/dating_site/profileupdate_interests.aspx.cs
@@ -13,5 +13,6 @@ namespace dating_site
         {
 
         }
+
     }
 }
--- dating_site/dating_site/profileupdate_interests.aspx.designer.cs
+++ dating_site/dating_site/profileupdate_interests.aspx.designer.cs
@@ -22,6 +22,15 @@ namespace dating_site {
         protected global::System.Web.UI.HtmlControls.HtmlForm regForm;
         
         /// <summary>
+        /// form_form control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Button form_form;
+        
+        /// <summary>
         /// form_gender control.
         /// </summary>
         /// <remarks>
@@ -56,14 +65,5 @@ namespace dating_site {
         /// To modify move field declaration from designer file to code-behind file.
         /// </remarks>
         protected global::System.Web.UI.WebControls.TextBox form_hobbies;
-        
-        /// <summary>
-        /// form_form control.
-        /// </summary>
-        /// <remarks>
-        /// Auto-generated field.
-        /// To modify move field declaration from designer file to code-behind file.
-        /// </remarks>
-        protected global::System.Web.UI.WebControls.Button form_form;
     }
 }
