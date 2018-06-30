d70da501586adbf539f78cff024fca3bdf601b38
Yuvraj Singh <uvnemk@gmail.com>
2018-06-30 04:08:27 -0700

    adding dropdown

--- dating_site/dating_site/mainpage.aspx.designer.cs
+++ dating_site/dating_site/mainpage.aspx.designer.cs
@@ -31,15 +31,6 @@ namespace dating_site {
         protected global::System.Web.UI.WebControls.Label Label1;
         
         /// <summary>
-        /// myDropdown control.
-        /// </summary>
-        /// <remarks>
-        /// Auto-generated field.
-        /// To modify move field declaration from designer file to code-behind file.
-        /// </remarks>
-        protected global::System.Web.UI.HtmlControls.HtmlGenericControl myDropdown;
-        
-        /// <summary>
         /// searchbar_text control.
         /// </summary>
         /// <remarks>
