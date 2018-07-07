5b119323eb23ab91a3a4367d195c70c808c9fc52
Yuvraj Singh <uvnemk@gmail.com>
2018-07-07 05:42:53 -0700

    adding posts

--- dating_site/dating_site/mainpage.aspx.cs
+++ dating_site/dating_site/mainpage.aspx.cs
@@ -14,14 +14,14 @@ namespace dating_site
     {
         protected void Page_Load(object sender, EventArgs e)
         {
-           button_friends.Attributes.Add("onclick", "return false;");
+            button_friends.Attributes.Add("onclick", "return false;");
         
-            Session["id"] =2;
+          /*  Session["id"] =2;
             Session["email"] = "auvnemk@gmail.com";
             Session["fname"] = "Yuvraj";
             Session["lname"] = "Singh";
-
-            // myImg.ImageUrl = Session["img"].ToString();
+            */
+             myImg.ImageUrl = Session["img"].ToString();
             Label1.Text = Session["fname"] + " " + Session["lname"];
             loadall();
                   
